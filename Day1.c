f9216783e43f05dabcbdb5f247bf2ce813943412
Sterling Collins <scstercollins@gmail.com>
2017-07-12 12:12:34 -0500

    Update Day1.c

--- Day1.c
+++ Day1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int largestnumber(int x, int y);
+int smallestnumber(int a, int b, int c);
 
 int main(void)
 {
@@ -10,12 +11,27 @@ int main(void)
 	int x;
 	int y;
 
+	int a;
+	int b;
+	int c;
+
+	printf("This 1rst function will take in 2 intergers and return the largest\n");
 	printf("Enter a number: ");
 	scanf_s("%d", &x);
 	printf("Enter a number: ");
 	scanf_s("%d", &y);
 
 	printf("Largest Number: %i\n", largestnumber(x, y));
+
+	printf("This 2nd function will take in 3 intergers and return the smallest\n");
+	printf("Enter a number: ");
+	scanf_s("%d", &a);
+	printf("Enter a number: ");
+	scanf_s("%d", &b);
+	printf("Enter a number: ");
+	scanf_s("%d", &c);
+
+	printf("Smallest Number: %i\n", smallestnumber(a, b, c));
 
 	return 0;
 }
@@ -29,5 +45,28 @@ int largestnumber(int x, int y)
 	else
 	{
 		return y;
+	}
+}
+
+int smallestnumber(int a, int b, int c)
+{
+	if(a < b)
+	{
+		if (a < c)
+		{
+			return a;
+		}
+
+	}
+	else if (b < a)
+	{
+		if (b < c)
+		{
+			return b;
+		}
+	}
+	else
+	{
+		return c;
 	}
 }
