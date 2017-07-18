ffff2f2b22032bd990ed2e169354c22444bee6b0
Sterling Collins <scstercollins@gmail.com>
2017-07-17 19:28:56 -0500

    Update Day2.c

--- Day2.c
+++ Day2.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include<stdlib.h>
-//#include "DataStructuresFunction.h"
 
 int smallint(int A[], int n);
 void insertionSort(int B[], int n);
@@ -9,55 +7,46 @@ void selectionSort(int Array[], int n);
 
 int main(void)
 {
+	// smallest integer
 	int Array[10] = { 323, 432, 4,321, 553, 2, 432, 342, 32, 10 };
-	int n = 10;
 	printf("smallestint: \n");
 	printf("Array: ");
 	printArray(Array, 10);
-	printf("Smallest int from Array is: %i\n", smallint(Array, n));
+	printf("Smallest int from Array is: %i\n", smallint(Array, 10));
 	printf("\n");
 
+	//insertion sort
 	int ArrayA[6] = { 16,8,13,4,12,6 };
 	int ArrayB[6] = { 7,9,7,10,8,6 };
 	int x = 6;
 	printf("insertionSort: \n");
 	printf("Unsorted ArrayA: ");
 	printArray(ArrayA, x);
-
 	insertionSort(ArrayA,x);
-
 	printf("Sorted ArrayA: ");
 	printArray(ArrayA, x);
 	printf("\n");
-
 	printf("Unsorted ArrayB: ");
 	printArray(ArrayB, x);
-
 	insertionSort(ArrayB,x);
-
 	printf("Sorted ArrayB: ");
 	printArray(ArrayB, x);
 	printf("\n");
 
+	//Selection Sort
 	int ArrayC[6] = { 12,10,11,4,12,5 };
 	int ArrayD[6] = { 7,2,23,12,12,21 };
 	int y = 6;
-
 	printf("selectionSort: \n");
 	printf("Unsorted ArrayC: ");
 	printArray(ArrayC, x);
-
 	selectionSort(ArrayC, x);
-
 	printf("Sorted ArrayC: ");
 	printArray(ArrayC, x);
 	printf("\n");
-
 	printf("Unsorted ArrayD: ");
 	printArray(ArrayD, x);
-
 	selectionSort(ArrayD, x);
-
 	printf("Sorted ArrayD: ");
 	printArray(ArrayD, x);
 	printf("\n");
@@ -67,25 +56,19 @@ int main(void)
 int smallint(int A[], int n)
 {
 	int smallestint = A[0];
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 1; i < n; i++)
 		if (A[i] < smallestint)
-		{
 			smallestint = A[i];
-		}
-	}
 	return smallestint;
 }
 
 void insertionSort(int B[], int n)
 {
-
 	int j, key, i;
 	for (j = 1; j < n; j++)
 	{
 		key = B[j];
 		i = j - 1;
-
 		while (i >= 0 && B[i] > key)
 		{
 			B[i + 1] = B[i];
@@ -98,9 +81,7 @@ void insertionSort(int B[], int n)
 void printArray(int Array[], int n)
 {
 	for (int i = 0; i < n; i++)
-	{
 		printf("%i ", Array[i]);
-	}
 	printf("\n");
 }
 
@@ -114,9 +95,7 @@ void selectionSort(int Array[], int n)
 		for (int i = (j + 1); i < n; i++)
 		{
 			if (Array[i] < Array[smallest])
-			{
 				smallest = i;
-			}
 		}
 		holder = Array[j];
 		Array[j] = Array[smallest];
